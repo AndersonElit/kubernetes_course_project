599c400d56ce8bcedbe88ddf83154bcea8694161
AndersonElit <rodriarri@gmail.com>
2024-10-02 23:38:02 -0500

    create springboot project

---
+++ kubernetesProject/src/test/java/com/kubernetes/project/kubernetesProject/KubernetesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.project.kubernetesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
