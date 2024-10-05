d6ff30ba7cf2954c4726868c9de76cf6d0f0c856
AndersonElit <rodriarri@gmail.com>
2024-10-05 16:05:06 -0500

    add secret config and env vars.

--- kubernetesProject/src/main/java/com/kubernetes/project/kubernetesProject/HelloController.java
+++ kubernetesProject/src/main/java/com/kubernetes/project/kubernetesProject/HelloController.java
@@ -1,14 +1,28 @@
 package com.kubernetes.project.kubernetesProject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${APPLICATION_PROFILE}")
+    private String applicationProfile;
+    @Value("${LOGGING_LEVEL}")
+    private String loggingLevel;
+    @Value("${USERNAME}")
+    private String userName;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello to kubernetes course!";
+        return "Hello to kubernetes course!, "
+                + "APPLICATION_PROFILE = applicationProfile, "
+                        .replace("applicationProfile", applicationProfile)
+                + "LOGGING_LEVEL = loggingLevel, "
+                    .replace("loggingLevel", loggingLevel)
+                + "USERNAME = userName"
+                    .replace("userName", userName);
     }
 
 }
